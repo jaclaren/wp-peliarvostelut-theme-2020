16797b07739b323de0a8117ed1783617fc1f1ed3
Jani <jani.claren@gmail.com>
2020-07-20 10:58:08 +0200

    Frontpage styling

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<main id="primary" class="col-xs-6 col-lg-4">
 
 		<?php
 		if ( have_posts() ) :
--- page.php
+++ page.php
@@ -14,8 +14,7 @@
 
 get_header();
 ?>
-
-	<main id="primary" class="site-main">
+	<main id="primary" class="">
 
 		<?php
 		while ( have_posts() ) :
