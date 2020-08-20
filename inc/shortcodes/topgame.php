b0aa92e8d5f9d5b829f55665197314f1c5911f7a
Jani <jani.claren@gmail.com>
2020-08-20 13:00:16 +0200

    Task: Etusivun nosto

--- inc/shortcodes/topgame.php
+++ inc/shortcodes/topgame.php
@@ -2,7 +2,10 @@
 
   add_shortcode('gamequery-topgame', 'peliarvostelut_net_topgame');
 
+
   function peliarvostelut_net_topgame($args) {
+    $games = \game_handler::get_with_highest_score_and_reviews();
+    $game = @$games[0];
     ?>
     <div class="row main">
     <div class="col-xs-12 col-sm-8 col-md--full">
@@ -10,7 +13,7 @@
       <iframe
         width="100%"
         height="315"
-        src="https://www.youtube.com/embed/tCI396HyhbQ"
+        src="<?php echo $game->get_youtube_trailer(); ?>"
         frameborder="0"
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
@@ -20,15 +23,15 @@
       <div class="box">
         <div class="row compilationcard--primary">
           <section class="col-xs-8 col-sm-12">
-            <h2>Death Stranding: Super long game name here</h2>
-            <div>4 arvostelua</div>
-            <a href="kooste.php" class="button button--solid">
-              Kooste
+            <h2><?php echo $game->get_title(); ?></h2>
+            <div><?php echo count($game->get_reviews()); ?> arvostelua</div>
+            <a href="<?php echo get_permalink($game->game_object); ?>" class="button button--solid">
+              <?php echo __('Kooste'); ?>
             </a>
           </section>
           <div class="col-xs-4 col-sm-12 first-sm">
             <div class="o-container o-container-xs--center o-container-xs--middle">
-              <div class="c-score" data-score="95"></div>
+              <div class="c-score" data-score="<?php echo $game->get_average_score(); ?>"></div>
             </div>
           </div>
         </div>
