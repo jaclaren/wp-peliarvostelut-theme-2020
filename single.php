bc2ed6ca4c3c73f15e28f7590ce83b931e11414a
Jani <jani.claren@gmail.com>
2020-08-22 08:12:56 +0200

    Added coverimage to post navigation

--- single.php
+++ single.php
@@ -18,10 +18,20 @@ get_header();
 
 			get_template_part( 'template-parts/content', get_post_type() );
 
+			$previous = get_previous_post();
+			$next = get_next_post();
+
+			if(!empty($next)) {
+				$next_game = new game($next);
+			}
+			if(!empty($previous)) {
+				$previous_game = new game($previous);
+			}
+
 			the_post_navigation(
 				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'peliarvostelut-net-2020-theme' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'peliarvostelut-net-2020-theme' ) . '</span> <span class="nav-title">%title</span>',
+					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'peliarvostelut-net-2020-theme' ) . '</span> </span> <img src="'.(!empty($previous_game) ? $previous_game->get_coverimg(true) : '').'" class="nav-coverimage" /> <span class="nav-title">%title</span>',
+					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'peliarvostelut-net-2020-theme' ) . '</span> <img src="'.(!empty($next_game) ? $next_game->get_coverimg(true) : '').'" class="nav-coverimage" /></span> <span class="nav-title">%title</span>',
 				)
 			);
 
