fd83b45d4f3a70a68aa87aff73cafc45bdec9adc
Jani <jani.claren@gmail.com>
2020-08-23 07:23:57 +0200

    Search icon now hidden on open

--- js/search.js
+++ js/search.js
@@ -1,3 +1,5 @@
+const searchButton = document.getElementById('main-search-icon');
+
 export default () => {
   document.getElementById('main-search-icon').addEventListener('click', () => openSearch())
   document.getElementById('search-close').addEventListener('click', () => closeSearch())
@@ -5,11 +7,13 @@ export default () => {
 
 function openSearch() {
   // document.getElementById("search__overlay").style.display = "block";
+  searchButton.classList.add('hidden')
   document.getElementById('search__overlay').classList.add('opened')
   document.getElementById("search-box-input").focus();
 }
 
 // Close the full screen search box
 function closeSearch() {
+  searchButton.classList.remove('hidden')
   document.getElementById('search__overlay').classList.remove('opened')
 }
