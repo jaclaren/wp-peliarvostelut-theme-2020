1eeac011146c2bf231caac5e72f957ef1bb57ce4
Jani <jani.claren@gmail.com>
2021-02-02 18:59:07 +0100

    Basic genre listing

--- template-parts/content-page-koosteet.php
+++ template-parts/content-page-koosteet.php
@@ -17,6 +17,13 @@
 <?php else: ?>
 	<?php do_shortcode('[gamequery-horizontal mode="latest" title="Uusimmat"]'); ?>
 	<div class="row">
+		<div class="col-xs-12 c-compilationlist__genregroup">
+			<?php foreach(\genre_group_handler::get_latest_groups() as $group): ?>				
+				<a href="<?php echo add_query_arg(['genre' => $group->get_id()]); ?>" class="button button--purple button--thin <?php echo $group->get_id() === intval(get_query_var('genre')) ? 'active' : ''; ?>" href="<?php get_permalink($group->get_id()); ?>"><?php echo $group->get_title(); ?></a>
+			<?php endforeach; ?>
+		</div>
+	</div>
+	<div class="row">
 		<div class="col-xs-12">
 			<?php the_widget('peliarvostelut_net_compilations_list', [], ['genre' => get_query_var('genre')]); ?>
 		</div>
