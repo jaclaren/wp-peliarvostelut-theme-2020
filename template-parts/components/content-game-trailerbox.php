c063c9ff359fa83b29330c32e90bf3096e9c5050
Jani <jani.claren@gmail.com>
2021-02-15 15:11:01 +0100

    Refactoring, visual styling

--- template-parts/components/content-game-trailerbox.php
+++ template-parts/components/content-game-trailerbox.php
@@ -1,12 +1,12 @@
-<div class="col-xs-8 col-md--full video-container__column" style="position:relative">
+<div class="col-xs-12 col-md-8 col-md--full video-container__column" style="position:relative">
     <iframe      
         width="100%"    
-        height="480"
+        height="100%"
         src="<?php echo @$args['game']->get_any_video(); ?>"
         frameborder="0"
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      </div>
-      <div class="col-xs-4 video-container__column">
+  </div>
+      <div class="col-xs-12 col-md-4 video-container__column">
       <?php $highlight_texts = $GLOBALS['game']->get_highlight_texts(); ?>
         <?php $highlight_texts_exist = count($highlight_texts) > 0; ?>
         <?php if($highlight_texts_exist): ?>
