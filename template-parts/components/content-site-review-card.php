f5bc51e51fafbdaae00ae1313d1204fddd3ddb3b
Jani <jani.claren@gmail.com>
2021-02-16 20:12:52 +0100

    Redesign

--- template-parts/components/content-site-review-card.php
+++ template-parts/components/content-site-review-card.php
@@ -1,22 +1,16 @@
-<?php 
-    // $related_review = \PANet\Utils::get_related_review_by_sitename($args['site']['name'], $args['reviews']);
-?>
-
 <?php if(!empty($args['site']['review'])): ?>
     <div class="c-reviewcard">    
-        <div class="c-reviewcard__score"><?php echo @$args['site']['review']->get_score(); ?></div>
-        <div class="c-reviewcard__title"><?php echo $args['site']['name']; ?></div>
+        <div class="c-reviewcard__score c-reviewcard__score--quality-<?php echo \PANet\Utils::get_quality_class_from_score(@$args['site']['review']->get_score()); ?>"><?php echo @$args['site']['review']->get_score(); ?></div>
+        <h3 class="c-reviewcard__title"><?php echo $args['site']['name']; ?></h3>
         <div class="c-reviewcard__creation"><?php echo \PANet\Utils::render_wp_post_creation_date($args['site']['review']->review_object); ?></div>                
-        <a href="#" rel="nofollow"><?php echo __('Lue'); ?></a>
+        <a class="c-reviewcard__link button button--dark1" href="<?php echo $args['site']['review']->get_url(); ?>" rel="nofollow"><?php echo __('Lue'); ?></a>
         
     </div>    
 <?php else: ?>
-    <div class="c-reviewcard--empty">
+    <div class="c-reviewcard c-reviewcard--empty">
         <div class="c-reviewcard__score">?</div>
-        <div class="c-reviewcard__title"><?php echo $args['site']['name']; ?></div>        
+        <h3 class="c-reviewcard__title"><?php echo $args['site']['name']; ?></h3>
         <div class="c-reviewcard__creation"><?php echo __('Ei arvosteltu'); ?></div>        
-
     </div>    
 <?php endif; ?>
 
-<p>
