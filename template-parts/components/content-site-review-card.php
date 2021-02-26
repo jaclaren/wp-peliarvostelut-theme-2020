8c67cc288ec2f0159c167937d4491da038aa65e5
Jani <jani.claren@gmail.com>
2021-02-26 07:54:54 +0100

    Task: Show review card quotes in mobile view

--- template-parts/components/content-site-review-card.php
+++ template-parts/components/content-site-review-card.php
@@ -1,9 +1,17 @@
-<?php if(!empty($args['site']['review'])): ?>
-    <div class="c-reviewcard">    
-        <div class="c-reviewcard__score c-reviewcard__score--quality-<?php echo \PANet\Utils::get_quality_class_from_score(@$args['site']['review']->get_score()); ?>"><?php echo @$args['site']['review']->get_score(); ?></div>
-        <h3 class="c-reviewcard__title"><?php echo $args['site']['name']; ?></h3>
-        <div class="c-reviewcard__creation"><?php echo \PANet\Utils::render_wp_post_creation_date($args['site']['review']->review_object); ?></div>                
-        <a class="c-reviewcard__link button button--dark1" href="<?php echo $args['site']['review']->get_url(); ?>" rel="nofollow"><?php echo __('Lue'); ?></a>
+<?php $review = $args['site']['review']; ?>
+
+<?php if(!empty($review)): ?>
+
+    <div class="c-reviewcard">            
+    
+        <div class="c-reviewcard__score c-reviewcard__score--quality-<?php echo \PANet\Utils::get_quality_class_from_score(@$review->get_score()); ?>"><?php echo @$review->get_score(); ?></div>
+        
+        <h3 class="c-reviewcard__title"><?php echo $args['site']['name']; ?></h3>                
+        <div class="c-reviewcard__creation"><?php echo \PANet\Utils::render_wp_post_creation_date($review->review_object); ?></div>                
+        <div class="c-reviewcard__cite"><cite>"<?php echo @$review->get_highlight_text()['text']; ?>"</cite></div>
+        
+        
+        <a class="c-reviewcard__link button button--dark1" href="<?php echo $review->get_url(); ?>" rel="nofollow"><?php echo __('Lue arvostelu'); ?></a>
         
     </div>    
 <?php else: ?>
